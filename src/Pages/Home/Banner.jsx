6fc171db069e5a3410810b5816d9c64b6970ea7a
Mahfuz <mahfuzru5634@gmail.com>
2025-11-17 12:42:37 +0600

    added hero section slider

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from "../../assets/banner/banner1.png";
+import img2 from "../../assets/banner/banner2.png";
+import img3 from "../../assets/banner/banner3.png";
+import { Carousel } from "react-responsive-carousel";
+import { GoArrowRight } from "react-icons/go";
 
 const Banner = () => {
-    return (
-        <div>
-            
+  return (
+    <Carousel
+      className="mt-5"
+      autoPlay={true}
+      infiniteLoop={true}
+      showThumbs={false}
+      showStatus={false}
+    >
+      <div className="relative">
+        <img src={img1} />
+
+        {/* Button container */}
+        <div className="absolute lg:top-120 lg:left-22 transform -translate-y-1/2 space-x-3">
+          <button className="rounded-full btn bg-primary px-4 py-2 shadow ">
+            Track Your Parcel <GoArrowRight></GoArrowRight>
+          </button>
+
+          <button className="rounded-md btn bg-white text-black px-4 py-2">
+            Be A Rider
+          </button>
         </div>
-    );
+      </div>
+      <div>
+        <img src={img2} />
+        <div className="absolute lg:top-120 lg:left-22 transform -translate-y-1/2 space-x-3">
+          <button className="rounded-full btn bg-primary px-4 py-2 shadow ">
+            Track Your Parcel <GoArrowRight></GoArrowRight>
+          </button>
+
+          <button className="rounded-md btn bg-white text-black px-4 py-2">
+            Be A Rider
+          </button>
+        </div>
+      </div>
+      <div>
+        <img src={img3} />
+        <div className="absolute lg:top-120 lg:left-22 transform -translate-y-1/2 space-x-3">
+          <button className="rounded-full btn bg-primary px-4 py-2 shadow ">
+            Track Your Parcel <GoArrowRight></GoArrowRight>
+          </button>
+
+          <button className="rounded-md btn bg-white text-black px-4 py-2">
+            Be A Rider
+          </button>
+        </div>
+      </div>
+    </Carousel>
+  );
 };
 
 export default Banner;
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
            {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl"><Logo></Logo></a>
+        <a className="text-xl ml-5"><Logo></Logo></a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
